910261f67caa306168ff330c37e311edabc9d30b
mariu <mariusbj@outlook.com>
2022-11-01 16:59:46 +0100

    ny commit

--- src/test/java/Test_WhenIsLeapYear.java
+++ src/test/java/Test_WhenIsLeapYear.java
@@ -1,31 +1,31 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class WhenIsLeapYear {
+public class Test_WhenIsLeapYear {
 
     @Test()
-    public void YearsDivisibleBy4ButNotBy100AreLeapYears() {
+    public void Test_YearsDivisibleBy4ButNotBy100AreLeapYears() {
 
         Assertions.assertTrue(Main.isLeapYear(4));
         Assertions.assertFalse(Main.isLeapYear(200));
 
     }
     @Test()
-    public void YearsDivisibleBy400AreLeapYears(){
+    public void Test_YearsDivisibleBy400AreLeapYears(){
 
         Assertions.assertTrue(Main.isLeapYear(400));
 
 
     }
     @Test()
-    public void YearsIndivisibleBy4AreNotLeapYears() {
+    public void Test_YearsIndivisibleBy4AreNotLeapYears() {
 
         Assertions.assertFalse(Main.isLeapYear(5));
 
 
     }
     @Test()
-    public void YearsDivisibleBy100ButNot400AreNotLeapYears() {
+    public void Test_YearsDivisibleBy100ButNot400AreNotLeapYears() {
 
         Assertions.assertFalse(Main.isLeapYear(100));
 
